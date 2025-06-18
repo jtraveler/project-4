dd09f0b3a2a6e2c249edb6d2cc63b3d8c0821f49
jtraveler <matthew.jtraveler@gmail.com>
2025-06-18 22:19:39 +0200

    feat: prepare Django project for Heroku deployment
    - Install gunicorn==20.1 for production server
    - Create Procfile for Heroku deployment
    - Update requirements.txt with current dependencies
    - Set DEBUG=False for production security
    - Add .herokuapp.com to ALLOWED_HOSTS for Heroku compatibility

--- prompts_manager/settings.py
+++ prompts_manager/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-1lv9*wwah72@s%)(mk^9a7uryxoq_v3m1j9pd8$6g&**#!u=(%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com']
 
 
 # Application definition

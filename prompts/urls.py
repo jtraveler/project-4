b68d1cfad7781432ea8de6bb0cecb69b8fa48a9b
jtraveler <matthew.jtraveler@gmail.com>
2025-06-20 10:17:58 +0200

    feat: organize URLs with namespaces

--- prompts/urls.py
+++ prompts/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
 
+app_name = 'prompts'
 urlpatterns = [
     path('', views.PromptList.as_view(), name='home'),
     path('<int:pk>/', views.PromptDetail.as_view(), name='prompt_detail'),
--- prompts_manager/urls.py
+++ prompts_manager/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('prompts.urls')),
+    path('', include('prompts.urls', namespace='prompts')),
 ]

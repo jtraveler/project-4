cd0a2bf5163b781a3e83f63d710fd1185a5be818
jtraveler <matthew.jtraveler@gmail.com>
2025-06-21 20:36:34 +0200

    feat: completed comment edit functionality with seperate page approach
    - Added missing reverse import to views.py
    - Created comment_edit.html template for edit form
    - Edit functionality now working with dedicated edit page
    - Users can edit their own comments with re-approval workflow

--- prompts/views.py
+++ prompts/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.views import generic
 from django.contrib import messages
 from django.http import HttpResponseRedirect
+from django.urls import reverse
 from .models import Prompt, Comment
 from .forms import CommentForm
 

a2434f9812ab2452e65e02d51187f54d0e18b44f
jtraveler <matthew.jtraveler@gmail.com>
2025-06-21 16:53:31 +0200

    feat: added comment edit and delete funcitionality - Added comment_edit and comment_delete views in views.py - Added URL patterns for edit/delete comment operations - Imported revrse function for proper URL redirection - Edit requires re-approval, delete removes comment completly

--- prompts/urls.py
+++ prompts/urls.py
@@ -4,6 +4,7 @@ from . import views
 app_name = 'prompts'
 urlpatterns = [
     path('', views.PromptList.as_view(), name='home'),
-    path('<slug:slug>/', views.prompt_detail, name='prompt_detail'),  # Ths is sett to use function-based view
-
+    path('<slug:slug>/', views.prompt_detail, name='prompt_detail'),
+    path('<slug:slug>/edit_comment/<int:comment_id>/', views.comment_edit, name='comment_edit'),
+    path('<slug:slug>/delete_comment/<int:comment_id>/', views.comment_delete, name='comment_delete'),
 ]
--- prompts/views.py
+++ prompts/views.py
@@ -43,3 +43,39 @@ def prompt_detail(request, slug):
             "comment_form": comment_form,
         },
     )
+
+def comment_edit(request, slug, comment_id):
+    """
+    View to edit comments
+    """
+    if request.method == "POST":
+        prompt = get_object_or_404(Prompt, slug=slug)
+        comment = get_object_or_404(Comment, pk=comment_id)
+        comment_form = CommentForm(data=request.POST, instance=comment)
+
+        if comment_form.is_valid() and comment.author == request.user:
+            comment = comment_form.save(commit=False)
+            comment.prompt = prompt
+            comment.approved = False
+            comment.save()
+            messages.add_message(request, messages.SUCCESS, 'Comment Updated!')
+        else:
+            messages.add_message(request, messages.ERROR, 'Error updating comment!')
+
+    return HttpResponseRedirect(reverse('prompts:prompt_detail', args=[slug]))
+
+
+def comment_delete(request, slug, comment_id):
+    """
+    View to delete comment
+    """
+    prompt = get_object_or_404(Prompt, slug=slug)
+    comment = get_object_or_404(Comment, pk=comment_id)
+
+    if comment.author == request.user:
+        comment.delete()
+        messages.add_message(request, messages.SUCCESS, 'Comment deleted!')
+    else:
+        messages.add_message(request, messages.ERROR, 'You can only delete your own comments!')
+
+    return HttpResponseRedirect(reverse('prompts:prompt_detail', args=[slug]))

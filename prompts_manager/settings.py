06c622dedc2ca8f420323ac7fad2bbad136256d3
jtraveler <matthew.jtraveler@gmail.com>
2025-06-21 09:13:21 +0200

    fix: cleaned up static files configuraton and added STATIC_ROOT
    - Removed duplicate STATIC_URL defintion
    - Added STATIC_ROOT for production deployement
    - Added STATIC_ROOT for production deployement
    - Organized static files settings proprly

--- prompts_manager/settings.py
+++ prompts_manager/settings.py
@@ -133,10 +133,7 @@ USE_I18N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
@@ -157,3 +154,4 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
+STATIC_ROOT = BASE_DIR / 'staticfiles'

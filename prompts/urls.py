9cd616041f453ee78aa442067b5890b24dceef3d
jtraveler <matthew.jtraveler@gmail.com>
2025-06-22 15:22:48 +0200

    feat: added URL patterns and prompt edit template with known issue - Added prompt edit/delete URL patterns to urls.py - Created prompt_edit.html template with crispy forms styline - URLs resolve correctly and edit form displays properly - Bug: I seem to be getting a 404 error after saving edited prompt

--- prompts/urls.py
+++ prompts/urls.py
@@ -6,6 +6,8 @@ urlpatterns = [
     path('', views.PromptList.as_view(), name='home'),
     path('collaborate/', views.collaborate_request, name='collaborate'),
     path('<slug:slug>/', views.prompt_detail, name='prompt_detail'),
+    path('<slug:slug>/edit/', views.prompt_edit, name='prompt_edit'),
+    path('<slug:slug>/delete/', views.prompt_delete, name='prompt_delete'),
     path('<slug:slug>/edit_comment/<int:comment_id>/', views.comment_edit, name='comment_edit'),
     path('<slug:slug>/delete_comment/<int:comment_id>/', views.comment_delete, name='comment_delete'),
 ]

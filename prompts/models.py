79df36d38dee3f7699076384267f1c8d7cd72281
jtraveler <matthew.jtraveler@gmail.com>
2025-06-24 00:46:25 +0200

    feat: intergrated django-taggit into prompts model - Added taggit to INSTALLED_APPS in settings - Imported TaggableManager in prompts models - Added tags field to Prompt model with TaggableManager - Created and ran database migratons for tagging system

--- prompts/models.py
+++ prompts/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.utils.text import slugify
 from cloudinary.models import CloudinaryField
+from taggit.managers import TaggableManager  
 
 
 # Add status choices
@@ -17,6 +18,7 @@ class Prompt(models.Model):
     status = models.IntegerField(choices=STATUS, default=0)
     created_on = models.DateTimeField(auto_now_add=True)
     updated_on = models.DateTimeField(auto_now=True)
+    tags = TaggableManager()
     
     class Meta:
         ordering = ['-created_on']

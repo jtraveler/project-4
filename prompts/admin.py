772d06dcd7783e90a55e2fd7a146785dc72ed15f
jtraveler <matthew.jtraveler@gmail.com>
2025-06-25 20:51:45 +0200

    feat: added likes display to admin inteface for better engagement tracking
    - Added number_of_likes to PromptAdmin list_display folowing DEV.to tutorial
    
    - Likes column now visible in admin prompt list for easy monitoring
    
    - Preserved existing admin customizations including tag_list and ordering

--- prompts/admin.py
+++ prompts/admin.py
@@ -1,10 +1,11 @@
+# prompts/admin.py
 from django.contrib import admin
 from django_summernote.admin import SummernoteModelAdmin
 from .models import Prompt, Comment, CollaborateRequest
 
 @admin.register(Prompt)
 class PromptAdmin(SummernoteModelAdmin):
-    list_display = ('title', 'slug', 'status', 'created_on', 'author', 'tag_list')
+    list_display = ('title', 'slug', 'status', 'created_on', 'author', 'tag_list', 'number_of_likes')
     search_fields = ['title', 'content', 'tags__name']  # Only one search_fields definition
     list_filter = ('status', 'created_on', 'author', 'tags')  # Added author back
     prepopulated_fields = {'slug': ('title',)}
@@ -23,14 +24,17 @@ class PromptAdmin(SummernoteModelAdmin):
 
 @admin.register(Comment)
 class CommentAdmin(admin.ModelAdmin):
-    list_display = ('author', 'body', 'prompt', 'created_on', 'approved')
-    list_filter = ('approved', 'created_on')
-    search_fields = ('author__username', 'body')
-    ordering = ['-created_on']
+    list_display = ['prompt', 'author', 'body', 'created_on', 'approved']
+    list_filter = ['approved', 'created_on']
+    search_fields = ['author__username', 'body']
+    actions = ['approve_comments']
+    
+    def approve_comments(self, request, queryset):
+        queryset.update(approved=True)
+    approve_comments.short_description = "Approve selected comments"
 
 @admin.register(CollaborateRequest)
 class CollaborateRequestAdmin(admin.ModelAdmin):
-    list_display = ('name', 'email', 'message', 'read', 'created_on')
-    list_filter = ('read', 'created_on')
-    search_fields = ('name', 'email')
-    ordering = ['-created_on']
+    list_display = ['name', 'email', 'message', 'read', 'created_on']
+    list_filter = ['read', 'created_on']
+    search_fields = ['name', 'email']
